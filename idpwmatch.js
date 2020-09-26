75614728d06ef77846e94d2a48029409bb66ac33
irin-kim <irenek91@korea.ac.kr>
2020-09-27 01:56:44 +0900

    0927 Commit last week practice and 1-1

---
+++ idpwmatch.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+</head>
+<body>
+    <script>
+        id = prompt('ID: ');
+        if(id=='irenek91'){
+            pw = prompt('PW: '){
+                alert('identified.');
+            }
+            else{
+                alert('unidentified.');
+            }
+        }
+        else{
+            alert('unidentified.');
+        }
+    </script>
+</body>
+</html>
---
+++ numberingfunction.js
@@ -0,0 +1,16 @@
+var numbering = function () {
+  i = 0;
+  while (i < 10) {
+    document.write(i);
+    i += 1;
+  }
+};
+numbering();
+
+function numbering() {
+  var i = 0;
+  while (i < 10) {
+    document.write(i);
+    i += 1;
+  }
+}
