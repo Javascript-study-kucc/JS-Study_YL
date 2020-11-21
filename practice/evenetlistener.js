f6b5c37f0af55ea82d030753041d5402e6c96eae
irin-kim <irenek91@korea.ac.kr>
2020-11-22 08:08:59 +0900

    1121 commit addeventlistener

--- practice/evenetlistener.js
+++ practice/evenetlistener.js
@@ -7,6 +7,14 @@ let myDiv = document.getElementById("myDiv");
 let myP2 = document.getElementById("myP2");
 let myDiv2 = document.getElementById("myDiv2");
 
-myP.addEventListener("click", function paintbox() {
-  myDiv.style.background = "lightblue";
-});
+myP.addEventListener("click", changebgcolor);
+
+function changebgcolor() {
+  myDiv.style.background = "skyblue";
+}
+
+myP.addEventListener("mouseover", changebordercolor);
+
+function changebordercolor() {
+  myDiv.style.background = "skyblue";
+}
