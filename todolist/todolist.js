7dc40f24665d10836270b5d0ff8f74e6ce27399f
irin-kim <irenek91@korea.ac.kr>
2020-11-27 02:58:44 +0900

    1126 commit todolist.js

--- todolist/todolist.js
+++ todolist/todolist.js
@@ -3,14 +3,16 @@
 let button = document.getElementById('button');
 let input = document.getElementById('input');
 let list = docoment.getElementById('list');
+let done = document.getElementById('done');
 
-button.addEventListener('click', addList);
+button.addEventListener('click', addNewTodo);
 //when button is clicked, add it to the list
-function addList{
-    //make li element
+function addNewTodo{
+    //make li element in temp
     let temp = document.createElement('li');
     //put input data to li
     temp.innerHTML = input.value;
     //add input to list
     list.appendChild(temp);
 }
+
